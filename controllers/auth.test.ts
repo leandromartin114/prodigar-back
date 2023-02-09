3643a759c791510f6c03bf1e3c322300eaed9783
Leandro Roldan <leandrom.roldan@gmail.com>
2023-02-09 09:46:23 -0300

    fix: Sendgrid, lib and controllers test ok

--- controllers/auth.test.ts
+++ controllers/auth.test.ts
@@ -8,22 +8,22 @@ test('Find Auth', async (t) => {
 	t.deepEqual(payload, out)
 })
 
-// test('Find or Create Auth', async (t) => {
-// 	const payload = false
-// 	const mockData = {
-// 		fullName: 'Leandro Roldan',
-// 		email: 'leandromartin_17@hotmail.com',
-// 		address: 'Calle Libertad',
-// 	}
-// 	const out = await findOrCreateAuth(mockData)
-// 	const res = out.isNewRecord
-// 	t.deepEqual(payload, res)
-// })
+test('Find or Create Auth', async (t) => {
+	const payload = false
+	const mockData = {
+		fullName: 'Leandro Roldan',
+		email: 'leandromartin_17@hotmail.com',
+		address: 'Calle Libertad',
+	}
+	const out = await findOrCreateAuth(mockData)
+	const res = out.isNewRecord
+	t.deepEqual(payload, res)
+})
 
-// test('Send Code', async (t) => {
-// 	const payload = typeof 12345
-// 	const email = 'leandromartin_17@hotmail.com'
-// 	const out = await sendCode(email)
-// 	const res = typeof out
-// 	t.deepEqual(payload, res)
-// })
+test('Send Code', async (t) => {
+	const payload = typeof 12345
+	const email = 'leandromartin_17@hotmail.com'
+	const out = await sendCode(email)
+	const res = typeof out
+	t.deepEqual(payload, res)
+})
--- controllers/item.test.ts
+++ controllers/item.test.ts
@@ -1,41 +1,41 @@
 import test from 'ava'
 import { createItem, updateItem, getItemtById, getItemsNearBy } from './item'
 
-// test('Create Item', async (t) => {
-// 	const payload = {
-// 		fullName: 'Leandro',
-// 		title: 'Mesa',
-// 		description: 'Mesa redonda blanca',
-// 		imgURL: 'img',
-// 		lat: -34,
-// 		lng: -68,
-// 		state: 'PUB',
-// 		email: 'leandromartin_17@hotmail.com',
-// 		userId: 1,
-// 	}
-// 	const mockData = {
-// 		fullName: 'Leandro',
-// 		title: 'Mesa',
-// 		description: 'Mesa redonda blanca',
-// 		lat: -34,
-// 		lng: -68,
-// 		state: 'PUB',
-// 		email: 'leandromartin_17@hotmail.com',
-// 	}
-// 	const out = await createItem(1, mockData)
-// 	const res = {
-// 		fullName: out.get("fullName"),
-// 		title: out.get("title"),
-// 		description: out.get("description"),
-// 		imgURL: out.get("imgURL"),
-// 		lat: out.get("lat"),
-// 		lng: out.get("lng"),
-// 		state: out.get("state"),
-// 		email: out.get("email"),
-// 		userId: out.get("userId"),
-// 	}
-// 	t.deepEqual(payload, res)
-// })
+test('Create Item', async (t) => {
+	const payload = {
+		fullName: 'Leandro',
+		title: 'Mesa',
+		description: 'Mesa redonda blanca',
+		imgURL: 'img',
+		lat: -34,
+		lng: -68,
+		state: 'PUB',
+		email: 'leandromartin_17@hotmail.com',
+		userId: 1,
+	}
+	const mockData = {
+		fullName: 'Leandro',
+		title: 'Mesa',
+		description: 'Mesa redonda blanca',
+		lat: -34,
+		lng: -68,
+		state: 'PUB',
+		email: 'leandromartin_17@hotmail.com',
+	}
+	const out = await createItem(1, mockData)
+	const res = {
+		fullName: out.get('fullName'),
+		title: out.get('title'),
+		description: out.get('description'),
+		imgURL: out.get('imgURL'),
+		lat: out.get('lat'),
+		lng: out.get('lng'),
+		state: out.get('state'),
+		email: out.get('email'),
+		userId: out.get('userId'),
+	}
+	t.deepEqual(payload, res)
+})
 
 test('Update Item', async (t) => {
 	const payload = {
